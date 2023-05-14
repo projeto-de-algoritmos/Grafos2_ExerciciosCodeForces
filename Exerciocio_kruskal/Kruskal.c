d3af0561b942a9cd0fcb5ab2b7eb7e15c86c9398
Mateus de Almeida <80906504+Mateuszinnn@users.noreply.github.com>
2023-05-14 15:59:24 -0300

    Arrumando erros

--- Exerciocio_kruskal/Kruskal.c
+++ Exerciocio_kruskal/Kruskal.c
@@ -5,13 +5,14 @@ using namespace std;
 #define N 200001
 
 struct r{
-    ll u, v, n, id;
+    ll u, v, w, id;
 } a[N];
 
-ll n,m,mst, fa[N], F[N], sz[N], l[N]; // mst minimal spanning tree
+ll n,m,mst, fa[N], f[N], sz[N], l[N]; // mst minimal spanning tree
 
 bool compare(r x, r y){
     return x.w<y.w;
+
 }
 
 ll add(ll u, ll v, ll w){
@@ -44,7 +45,7 @@ int main(){
         sz[i] = 1;
         fa[i] = a[i].id=i;
     }         
-    Sort(a+1,a+m+1, compare);
+    sort(a+1,a+m+1, compare);
     for(int i = 1; i <=m; i++){
         f[a[i].id] = add(a[i].u , a[i].v, a[i].w);
     }
@@ -52,3 +53,14 @@ int main(){
         cout<<mst+f[i]<<endl;
     }
 }
+
+/*
+5 7
+1 2 3
+1 3 1
+1 4 5
+2 3 2
+2 5 3
+3 4 2
+4 5 4
+*/
